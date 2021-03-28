132153246af5bff4ec83aa516ea9c24cc86a65d8
ac2pic <ac2pic@gmail.com>
2021-03-28 14:34:42 -0500

    Restructured mod

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ export default class Emilie {
         this.mod = mod;
     }
 
+    preload() {
+        if (modloader.installMods.get("char-select")) {
+            console.log("char-select is installed. :leaCheese:");
+        }
+    }
+
     async registerResourceGenerators() {
         await UtilityResourceManager.addResourceGenerator('custom-character-config', async () => {
             const response = await fetch(`/${this.mod.baseDirectory}character-config.json`);
